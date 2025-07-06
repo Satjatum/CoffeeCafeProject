37da35a3f60e3ed6f48c46e3075941bde04b8021
kazaza021 <pooklnw.2546@gmail.com>
2025-07-06 14:51:40 +0700

    Add project files.

---
+++ CoffeeCafeProject/FrmMenu.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows.Forms;
+
+namespace CoffeeCafeProject
+{
+    public partial class FrmMenu : Form
+    {        
+        public FrmMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmMenu_Load(object sender, EventArgs e)
+        {
+            pbMenuImage.Image = null;
+            tbMenuId.Clear();
+            tbMenuName.Clear();
+            tbMenuPrice.Clear();
+            btSave.Enabled = true;
+            btUpdate.Enabled = false;
+            btDelete.Enabled = false;
+        }
+    }
+}
